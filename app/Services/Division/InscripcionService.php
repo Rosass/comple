3fcbf7c4385efbdbdbf85b3bd761dcacf71f2593
rosas <luisrosas950@gmail.com>
2020-10-03 01:13:02 -0500

    inicio

---
+++ app/Services/Division/InscripcionService.php
@@ -0,0 +1,199 @@
+<?php
+namespace App\Services\Division;
+
+class InscripcionService
+{
+
+    protected $inscripcionModel;
+    protected $alumnoModel;
+
+    function __construct()
+    {
+        $this->inscripcionModel = new \App\Models\Division\InscripcionModel();
+        $this->alumnoModel = new \App\Models\AlumnoModel();
+    }
+
+    /**
+     * Obtiene las inscripciónes por estatus de la BD
+     * @return object
+     */
+    public function getInscripcionesPorEstatus($estatus)
+	{   
+       return $this->inscripcionModel->getInscripcionesPorEstatus($estatus);
+    }
+    
+    /**
+     * Esta función obtiene inscripciones filtradas por actividad
+     * 
+     * @param int $id_actividad
+     * @param bool $estatus
+     * @return string
+     */
+    public function getInscripcionesPorActividadYEstatus($id_actividad, $estatus)
+    {
+        
+        // El id_actividad 0 significa que se selecciono "TODAS LAS ACTIVIDADES"
+        if($id_actividad == 0)
+        {
+            // Se obtienen las inscripciones
+            $inscripciones = $this->inscripcionModel->getInscripcionesPorEstatus($estatus);
+        }
+        else
+        {
+            // Se obtienen las inscripciones
+            $inscripciones = $this->inscripcionModel->getInscripcionesPorActividadYEstatus($id_actividad, $estatus);
+        }
+        
+        // Se unen las inscripciones con los datos del alumno obtenidos de la BD "alumnos"
+        $inscripciones_aux = $this->unirRegistros($inscripciones);
+
+        // Se crea el html de respuesta
+        $inscripciones_html = '';
+        
+        foreach ($inscripciones_aux as $key => $inscripcion)
+        {
+
+            $inscripciones_html .= '<tr>' .
+                                        '<th scope="row">' . ($key + 1) . '</th>' .
+                                        '<td>' . $inscripcion->num_control . '</td>' .
+                                        '<td style="width:30%;">' . $inscripcion->nombre . " " . $inscripcion->ap_paterno . " " . $inscripcion->ap_materno . '</td>' .
+                                        '<td>' . $inscripcion->carrera . '</td>' .
+                                        '<td>' . $inscripcion->semestre . '</td>' .
+                                        '<td>' . $inscripcion->descripcion_periodo . '</td>' .
+                                        '<td>' . $inscripcion->nombre_actividad . '</td>' .
+                                        '<td>' . $inscripcion->telefono . '</td>' .
+                                        '<td>' . $inscripcion->fecha_inscripcion . '</td>' .
+                                        '<td style="width:15%;">' .
+                                            '<div class="d-flex flex-column">' .
+                                                '<!--  Editar inscripción -->' .
+                                                '<a class="btn btn-warning btn-sm btn-block mb-1" href="' . base_url("division/inscripciones/editar/" . $inscripcion->id_inscripcion) . '"><i class="fas fa-pen"></i> Editar</a>' .
+                                                '<!-- Eliminar inscripción -->' .
+                                                '<form action="' . base_url('division/inscripciones/cambiar-estatus') . '" method="POST">' .
+                                                    '<input type="hidden" name="id_inscripcion" value="' . $inscripcion->id_inscripcion . '">' .
+                                                    '<button type="submit" class="btn btn-danger btn-sm btn-block btnEnviarFormulario" data-no_control="' . $inscripcion->num_control . '" ><i class="fas fa-trash"></i> Eliminar</button>' .
+                                                '</form>' .
+                                        '</td>' .
+                                    '</tr>';
+        }
+        return $inscripciones_html;
+    }
+
+    /**
+     * Guarda una nueva inscripción en la BD
+     * @param array $datos
+     * @return array
+     */
+    public function guardar($datos)
+    {
+        $alumno = $this->alumnoModel->getAlumnoPorNoControl($datos['num_control']);
+
+        if($alumno != NULL)
+        {
+            $inscripcion = $this->inscripcionModel->getInscripcionPorNoControlPeriodoYActividad($datos['num_control'], $datos['periodo'], $datos['id_actividad']);
+
+            if($inscripcion == NULL)
+            {
+                if ($this->inscripcionModel->guardar($datos))
+                    return ["exito" => true, "msj" => "Inscripción agregada con exito."];
+                else
+                    return ["exito" => false, "msj" => "Algo salio mal, intentalo de nuevo."];
+            }
+            else 
+            {
+                return ["exito" => false, "msj" => "Ya se encuentra registrada una inscripción con la actividad selecionada!."];
+            }
+        }
+        else
+        {
+            return ["exito" => false, "msj" => "No se encontro el número de control proporcionado."];
+        }
+
+    }
+
+    /**
+     * Actualiza los datos de una inscripción en la BD
+     * @param array $datos
+     * @return array
+     */
+    public function actualizar($id_inscripcion, $datos)
+    {
+        $inscripcion = $this->inscripcionModel->getInscripcionPorId($id_inscripcion);
+
+        if($inscripcion != NULL)
+        {
+            
+            // Si la actividad sigue siendo la misma (que no se cambió) se elimina de los datos a actualizar
+            if ($inscripcion->id_actividad == $datos['id_actividad'])
+            {
+                unset($datos['id_actividad']);
+            }
+            else
+            {
+                $inscripcion_aux = $this->inscripcionModel->getInscripcionPorNoControlPeriodoYActividad($inscripcion->num_control, $datos['periodo'], $datos['id_actividad']);
+
+                if($inscripcion_aux != NULL)
+                {   
+                    return ["exito" => false, "msj" => "Ya se encuentra registrada una inscripción con la actividad selecionada!."];
+                }
+            }
+
+            // Se actualizan los datos
+            if ($this->inscripcionModel->actualizar($id_inscripcion, $datos))
+            {
+                return ["exito" => true, "msj" => "Datos actualizados con exito."];
+            }
+            else
+            {
+                return ["exito" => false, "msj" => "No se actualizó ningun campo."];
+            }
+            
+        }
+        else 
+        {
+            return ["exito" => false, "msj" => "Id de actividad no válido!."];
+        }
+       
+    }
+
+    public function cambiarEstatus($id_inscripcion)
+    {
+        $inscripcion = $this->inscripcionModel->getInscripcionPorId($id_inscripcion);
+
+        $nuevo_estatus = ($inscripcion->estatus == true) ? false : true;
+        $datos = [ 'estatus' => $nuevo_estatus ];
+
+        if ($this->inscripcionModel->actualizar($id_inscripcion, $datos))
+            return ["exito" => true, "msj" => "Datos actualizados con exito."];
+        else
+            return ["exito" => false, "msj" => "No se actualizó ningun campo."];
+    }
+
+    public function getInscripcionPorId($id_inscripcion)
+    {
+        return $this->inscripcionModel->getInscripcionPorId($id_inscripcion);
+    }
+
+    /**
+     * Esta función une registros de 2 BD mediante el numero de control del alumno
+     * @param $array1
+     * @param $array2
+     * @return array
+     */
+    function unirRegistros($inscripciones)
+    {
+        if(count($inscripciones) > 0)
+        {
+            for($r = 0; $r < count($inscripciones); $r++)
+            {
+                $array1 = (array) $this->alumnoModel->getAlumnoPorNoControl($inscripciones[$r]->num_control);
+                $array2 = (array) $inscripciones[$r];
+                $array_merge[$r] = (object) array_merge($array1, $array2);
+            }
+            return $array_merge;
+        }
+        else
+        {
+            return $inscripciones;
+        }
+    }
+}
---
+++ public/js/script.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+
+	// DECLARACIÓN DE VARIABLES
+	const URL_BASE = 'https://ac.pochutla.tecnm.mx/';
+	const selectActividad = document.getElementById('selectActividad');
+	const tbodyTablaInscripciones = document.getElementById('tbodyTablaInscripciones');
+	
+    const traduccion_datatable_esp = {
+		"sProcessing": "Procesando...",
+		"sLengthMenu": "Mostrar _MENU_",
+		"sZeroRecords": "No se encontraron resultados",
+		"sEmptyTable": "Ningún dato disponible en esta tabla",
+		"sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+		"sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+		"sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+		"sInfoPostFix": "",
+		"sSearch": "Buscar:",
+		"sUrl": "",
+		"sInfoThousands": ",",
+		"sLoadingRecords": "Cargando...",
+		"oPaginate": {
+			"sFirst": "Primero",
+			"sLast": "Último",
+			"sNext": "Siguiente",
+			"sPrevious": "Anterior"
+		}
+		
+    };
+    
+    // Se asigna el idioma español al plugin Datatables
+    $('#tablaActividades, #tablaInscripciones, #tablaPeriodos, #tablaTiposActividades, #tablaResponsables, #tablaJefes, #tablaAreas, #tablaTiposUsuarios, #tablaUsuarios').DataTable({ "language": traduccion_datatable_esp });
+	
+	// Listener para filtrar las inscripciones por actividad mediante fetch
+    selectActividad.addEventListener('change', (event) =>{
+		const data = new FormData();
+		data.append('id_actividad', event.target.value);
+
+		// Se limpia la tabla
+		$('#tablaInscripciones').DataTable().clear().destroy();
+
+		// Se añade el spinner de cargando
+		tbodyTablaInscripciones.innerHTML = `<tr>
+												<td colspan="9">
+													<div class="text-center"><div class="spinner-border text-primary" role="status"></div></div>
+												</td>
+                            				</tr>`;
+                            				
+        // Se hace la consulta al servidor mediante Fetch
+		fetch(URL_BASE + 'division/inscripciones',{method: "POST", headers:{"X-Requested-With": "XMLHttpRequest"}, body: data})
+		.then(function (response)
+		{
+			if (response.ok) return response.json();
+			else throw "Woops, Algo salio mal!";
+		})
+		.then(function (data)
+		{
+			// Se añaden los nuevos datos a la tabla
+			tbodyTablaInscripciones.innerHTML = data;
+			
+			// Se vuelve a asignar el plugin "Datatables" para que vuelva a paginar
+			$('#tablaInscripciones').DataTable({ "language": traduccion_datatable_esp });
+		})
+		.catch(error => console.log(error));
+	});
+})
