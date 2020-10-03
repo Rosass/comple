e26ead103d7b591432f3721a2e4fc4f6916e05af
Alvaro <sanchezrmz410@gmail.com>
2020-10-03 03:20:03 -0500

    modulo jefemsj

--- app/Views/Admin/Jefes/editar.php
+++ app/Views/Admin/Jefes/editar.php
@@ -66,14 +66,6 @@
 			</form>
 		</div>
 	</div>
-    <!-- Mensajes de error -->
-	<?php if($session->getFlashdata('error')) : ?>
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            <?= $session->getFlashdata('error') ?>
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        </div>
-	<?php endif ?>
-	
 <script>
     const btnEnviarFormulario = document.querySelectorAll(".btnEnviarFormulario");
 
