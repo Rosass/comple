874d44553e315b464617768612d9d10ae2c4cf1d
rosas <luisrosas950@gmail.com>
2021-03-18 16:23:03 -0600

    cambio editar actividad HORAS

--- app/Views/Division/Actividades/editar.php
+++ app/Views/Division/Actividades/editar.php
@@ -46,7 +46,7 @@
 							<div class="col-md-3">
 								<div class="form-group">
 									<label for="horas">NÚMERO DE HORAS</label>
-									<input type="number" class="form-control" id="horas" name="horas" required value="<?= $actividad->horas ?>" min="0">
+									<input type="number" class="form-control" id="horas" name="horas" value="<?= $actividad->horas ?>" min="0">
 								</div>
 							</div>
 						</div>
