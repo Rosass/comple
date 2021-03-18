8394dd9ae56613322d003411a88026f0198d9dda
rosas <luisrosas950@gmail.com>
2021-03-18 16:24:23 -0600

    cambio editar actividad HORARIO

--- app/Views/Division/Actividades/editar.php
+++ app/Views/Division/Actividades/editar.php
@@ -87,7 +87,7 @@
 							<div class="col-md-8">
 								<div class="form-group">
 									<label for="horario">HORARIO</label>
-									<input type="horario" class="form-control text-uppercase" id="horario" name="horario" required value="<?= $actividad->horario ?>">
+									<input type="horario" class="form-control text-uppercase" id="horario" name="horario" value="<?= $actividad->horario ?>">
 									<small>Los horarios debes separarlos por punto y coma ';'.</small>
 								</div>
 							</div>
