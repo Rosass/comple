062533e4ca0c728c6e7f2de44fece3c2f70a7be0
rosas <luisrosas950@gmail.com>
2021-03-22 13:12:25 -0600

    cambio Responsable calificacion no aprobado 5

--- app/Views/Responsable/Calificaciones/listar.php
+++ app/Views/Responsable/Calificaciones/listar.php
@@ -58,7 +58,7 @@
                                 <?php if($alumno['valor_numerico'] < 0)  ?>                   
                                 <td style="width:8%;">             
                                     <div class="d-flex flex-column">
-                                    <?php if($alumno['valor_numerico'] < 0) : ?>
+                                    <?php if($alumno['valor_numerico'] == 0) : ?>
                                         <a class="btn btn-info btn-sm btn-block mb-0" href="<?= base_url("responsables/evaluacion/$alumno[num_control]/$alumno[id_inscripcion]/$id_actividad") ?>"><i class="fas fa-file-alt"></i> Evaluacion Desempeño</a>                                   
                                     <?php endif ?>
                                     <?php if ( $alumno['valor_numerico'] > 0) echo 'CALIFICACIÓN ASIGNADA';?>
