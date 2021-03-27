2498dd76f993120b96b5f776fc095436763fb0b5
rosas <luisrosas950@gmail.com>
2021-03-27 13:41:17 -0600

    cambio ortografia jefe tabla

--- app/Views/Jefes/Alumnos/lista.php
+++ app/Views/Jefes/Alumnos/lista.php
@@ -31,17 +31,17 @@ $session = session(); ?>
                 <table class="table table-hover table-light table-striped shadow-lg" id="tablaActividades">
                     <thead class="bg-color-tec-blue border-top-0 table-sm text-center text-white">
                         <tr>
-                            <th scope="col" colspan="13" class="border-top-0 p-2"><h3 class="mb-0">Calificacion </h3></th>
+                            <th scope="col" colspan="13" class="border-top-0 p-2"><h3 class="mb-0">CALIFICACIÓN </h3></th>
                         </tr>
                         <tr>
-                            <th scope="col" class="border-top-0">No</th>
-                            <th scope="col" class="border-top-0">No.Control</th>
-                            <th scope="col" class="border-top-0">Nombre</th>
-                            <th scope="col" class="border-top-0">Carrera</th>
-                            <th scope="col" class="border-top-0">Semestre</th>
-                            <th scope="col" class="border-top-0">Calificacion</th>
-                            <th scope="col" class="border-top-0">Nivel Desempeño</th>
-                            <th scope="col" class="border-top-0">Accion</th>
+                            <th scope="col" class="border-top-0">NO</th>
+                            <th scope="col" class="border-top-0">NO.CONTROL</th>
+                            <th scope="col" class="border-top-0">NOMBRE</th>
+                            <th scope="col" class="border-top-0">CARRERA</th>
+                            <th scope="col" class="border-top-0">SEMESTRE</th>
+                            <th scope="col" class="border-top-0">CALIFICAÓN</th>
+                            <th scope="col" class="border-top-0">NIVEL DESEMPEÑO</th>
+                            <th scope="col" class="border-top-0">ACCIÓN</th>
                         </tr>
                     </thead>
                     <tbody class="text-center table-sm">
